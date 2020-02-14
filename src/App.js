17fca9ed002fd5834d17a647622b42fc64d7657c
Sergio <sergio@Sergios-MBP.home>
2020-02-13 22:15:06 -0500

    first commit for this page transition practice

--- src/App.js
+++ src/App.js
@@ -1,26 +1,68 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.scss'
+import { Route } from 'react-router-dom'
+import { CSSTransition } from 'react-transition-group'
+import { gsap } from 'gsap'
+
+import Home from './pages/Home'
+import About from './pages/About'
+
+import Header from './components/Header'
+
+const routes = [ { path: '/', name: 'Home', Component: Home }, { path: '/about', name: 'About', Component: About } ]
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const onEnter = (node) => {
+    // enter animation
+    // console.log(node)
+		gsap.from([ node.children[0].firstElementChild, node.children[0].lastElementChild ], 0.6, {
+			y: 30,
+			delay: 0.6,
+			ease: 'power3.InOut',
+			opacity: 0,
+			stagger: {
+				amount: 0.6
+			}
+		})
+	}
+
+	const onExit = (node) => {
+    // exit animation
+    // console.log(node)
+    gsap.to([ node.children[0].firstElementChild, node.children[0].lastElementChild ], 0.6, {
+			y: -30,
+			ease: 'power3.InOut',
+			stagger: {
+				amount: 0.6
+			}
+		})
+	}
+
+	return (
+		<div>
+			<Header />
+			<div className="container">
+				{routes.map(({ path, Component }) => (
+					<Route key="name" path={path} exact>
+						{({ match }) => (
+							<CSSTransition
+								in={match != null}
+								timeout={1200}
+								classNames="page"
+								unmountOnExit
+								onExit={onExit}
+								onEnter={onEnter}
+							>
+								<div className="page">
+									<Component />
+								</div>
+							</CSSTransition>
+						)}
+					</Route>
+				))}
+			</div>
+		</div>
+	)
 }
 
-export default App;
+export default App
